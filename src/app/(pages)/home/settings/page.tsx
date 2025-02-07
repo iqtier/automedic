33f0ca359d00276170935d6fa15ae140ad3f2865
Iqtier Uddin Ahammad <40275201+iqtier@users.noreply.github.com>
2025-02-07 14:14:45 -0330

    Update page.tsx

--- src/app/(pages)/home/settings/page.tsx
+++ src/app/(pages)/home/settings/page.tsx
@@ -1,9 +1,228 @@
-import React from 'react'
+"use client"
+import {
+  useState
+} from "react"
+import {
+  toast
+} from "sonner"
+import {
+  useForm
+} from "react-hook-form"
+import {
+  zodResolver
+} from "@hookform/resolvers/zod"
+import * as z from "zod"
+import {
+  cn
+} from "@/lib/utils"
+import {
+  Button
+} from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {
+  Input
+} from "@/components/ui/input"
+import LocationSelector from "@/components/ui/location-input"
+import {
+  CloudUpload,
+  Paperclip
+} from "lucide-react"
+import {
+  FileInput,
+  FileUploader,
+  FileUploaderContent,
+  FileUploaderItem
+} from "@/components/ui/file-upload"
 
-const page = () => {
+const formSchema = z.object({
+  businame: z.string(),
+  location: z.tuple([z.string(), z.string().optional()]),
+  roadname: z.string(),
+  postal: z.string(),
+  logo: z.string()
+});
+
+export default function MyForm() {
+
+  const [countryName, setCountryName] = useState < string > ('')
+  const [stateName, setStateName] = useState < string > ('')
+
+  const [files, setFiles] = useState < File[] | null > (null);
+
+  const dropZoneConfig = {
+    maxFiles: 5,
+    maxSize: 1024 * 1024 * 4,
+    multiple: true,
+  };
+  const form = useForm < z.infer < typeof formSchema >> ({
+    resolver: zodResolver(formSchema),
+
+  })
+
+  function onSubmit(values: z.infer < typeof formSchema > ) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
   return (
-    <div>settings page</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
+        
+        <FormField
+          control={form.control}
+          name="businame"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Business Name</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="Business name "
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>This is your business name</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+           <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Select Country</FormLabel>
+                  <FormControl>
+                  <LocationSelector
+                    onCountryChange={(country) => {
+                      setCountryName(country?.name || '')
+                      form.setValue(field.name, [country?.name || '', stateName || ''])
+                    }}
+                    onStateChange={(state) => {
+                      setStateName(state?.name || '')
+                      form.setValue(field.name, [form.getValues(field.name)[0] || '', state?.name || ''])
+                    }}
+                  />
+                  </FormControl>
+                  <FormDescription>If your country has states, it will be appear after selecting country</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+        
+        <div className="grid grid-cols-12 gap-4">
+          
+          <div className="col-span-6">
+            
+        <FormField
+          control={form.control}
+          name="roadname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Road Name</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="Road Name"
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>This is your public display name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+          <div className="col-span-6">
+            
+        <FormField
+          control={form.control}
+          name="postal"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Postal code</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="Postal Code"
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>This is your public display name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </div>
+          
+        </div>
+        
+            <FormField
+              control={form.control}
+              name="logo"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Select File</FormLabel>
+                  <FormControl>
+                    <FileUploader
+                      value={files}
+                      onValueChange={setFiles}
+                      dropzoneOptions={dropZoneConfig}
+                      className="relative bg-background rounded-lg p-2"
+                    >
+                      <FileInput
+                        id="fileInput"
+                        className="outline-dashed outline-1 outline-slate-500"
+                      >
+                        <div className="flex items-center justify-center flex-col p-8 w-full ">
+                          <CloudUpload className='text-gray-500 w-10 h-10' />
+                          <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                            <span className="font-semibold">Click to upload</span>
+                            &nbsp; or drag and drop
+                          </p>
+                          <p className="text-xs text-gray-500 dark:text-gray-400">
+                            SVG, PNG, JPG or GIF
+                          </p>
+                        </div>
+                      </FileInput>
+                      <FileUploaderContent>
+                        {files &&
+                          files.length > 0 &&
+                          files.map((file, i) => (
+                            <FileUploaderItem key={i} index={i}>
+                              <Paperclip className="h-4 w-4 stroke-current" />
+                              <span>{file.name}</span>
+                            </FileUploaderItem>
+                          ))}
+                      </FileUploaderContent>
+                    </FileUploader>
+                  </FormControl>
+                  <FormDescription>Select a file to upload.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
-
-export default page
