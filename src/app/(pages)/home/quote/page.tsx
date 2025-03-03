5fb525c661f872cd53d4510ac5321181e8e0e1ed
Iqtier Uddin Ahammad <iqt.taki@gmail.com>
2025-03-02 23:31:54 -0330

    Remove unused service fetching in Quote page component

--- src/app/(pages)/home/quote/page.tsx
+++ src/app/(pages)/home/quote/page.tsx
@@ -2,7 +2,7 @@ import { getAllServices } from '@/app/actions/serviceActions';
 import React from 'react'
 
 const page = async () => {
-   const services = await getAllServices();
+ 
   return (
     <div>Quote page</div>
   )
