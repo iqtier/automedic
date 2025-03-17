4020ef924a103406d0ef15e42eb5ca4bfaec3918
Iqtier Uddin Ahammad <40275201+iqtier@users.noreply.github.com>
2025-03-17 15:43:03 -0230

    Update business-setup-form.tsx

--- src/app/(component)/Authentication/business-setup-form.tsx
+++ src/app/(component)/Authentication/business-setup-form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { forwardRef, useState, useTransition } from "react";
+import { forwardRef, useState, useTransition,useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
